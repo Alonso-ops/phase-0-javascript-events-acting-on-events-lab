b91ef6a5596086e823b21388e6427034845830e3
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-01-20 22:37:19 -0700

    Acting On Events

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+  
+    
+    let leftNumbers = dodger.style.left.replace("px", ""); 
+    let left = parseInt(leftNumbers, 10);
+  
+    
+    if (left < 360) { 
+      dodger.style.left = `${left + 7}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
